3ced2c0f5cb956ac456d04679b7c05de02d55a27
Chandan Kumar <chandanios@loopmethods.com>
2021-03-25 20:29:54 +0530

    code templates add on server

--- hans_bank_system/customer/views.py
+++ hans_bank_system/customer/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.contrib.auth.models import User
+
+
+from django.db.models import Q
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+
+
+
+
+
+
+
